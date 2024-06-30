55c71f2d78146e30f0d82037efd4809b36ee17ce
Matyrobbrt <matyrobbrt@gmail.com>
2024-07-01 00:37:08 +0300

    Use a more straightforward argument detection method

--- src/main/java/net/neoforged/serverstarterjar/Main.java
+++ src/main/java/net/neoforged/serverstarterjar/Main.java
@@ -16,7 +16,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static final char ESCAPE = (char) 92; // \\
@@ -39,16 +38,20 @@ public class Main {
 
     public static void main(String[] $) throws Throwable {
         final var argsFile = getArgsFile();
-        final var args = Files.readAllLines(argsFile);
-        final var copy = new ArrayList<>(args);
+        final var args = Files.readAllLines(argsFile).stream()
+                .flatMap(arg -> toArgs(arg).stream()).collect(Collectors.toCollection(ArrayList::new));
 
-        final var pathArg = find(args, copy, arg -> arg.startsWith("-p "))
-                .findFirst().orElseThrow().substring("-p ".length());
+        final var pathArg = findValue(args, "-p");
+        if (pathArg == null) {
+            System.err.println("Could not find module path (specified by -p)");
+            System.exit(1);
+            return;
+        }
+
         final var bootPath = installModulePath(getModulePath(pathArg));
 
-        copy.indexOf("-p");
-        find(args, copy, arg -> arg.startsWith("--add-opens "))
-                .map(arg -> arg.substring("--add-opens ".length()).split("="))
+        findValues(args, "--add-opens").stream()
+                .map(arg -> arg.split("="))
                 .forEach(toOpen -> Agent.instrumentation.redefineModule(
                         bootPath.layer().findModule(toOpen[0].split("/")[0]).orElseThrow(),
                         Set.of(),
@@ -57,8 +60,8 @@ public class Main {
                         Set.of(),
                         Map.of()
                 ));
-        find(args, copy, arg -> arg.startsWith("--add-exports "))
-                .map(arg -> arg.substring("--add-exports ".length()).split("="))
+        findValues(args, "--add-exports").stream()
+                .map(arg -> arg.split("="))
                 .forEach(toExport -> Agent.instrumentation.redefineModule(
                         bootPath.layer().findModule(toExport[0].split("/")[0]).orElseThrow(),
                         Set.of(),
@@ -70,38 +73,41 @@ public class Main {
 
         BOOT_LAYER.invokeExact(bootPath.layer());
 
-        find(args, copy, arg -> arg.startsWith("-D"))
+        var sysProps = args.stream().filter(arg -> arg.startsWith("-D")).toList();
+        sysProps.forEach(args::remove);
+        sysProps.stream()
                 .map(arg -> arg.substring("-D".length()).split("=", 2))
                 .forEach(arg -> System.setProperty(arg[0], arg[1]));
 
-        // The args file specifies "--add-modules ALL-MODULE-PATH" which is completely useless now
-        copy.remove("--add-modules ALL-MODULE-PATH");
+        // The args file specifies "--add-modules ALL-MODULE-PATH" which is completely useless now, so we ignore it
+        findValue(args, "--add-modules");
 
-        final var mainName = copy.remove(0);
-        final var mainSplit = mainName.split("\\.");
-        final var mainSplitByDot = new ArrayList<>(Arrays.asList(mainSplit));
-        mainSplitByDot.remove(mainSplitByDot.size() - 1);
-        final var mainPkg = String.join(".", mainSplitByDot);
+        final var mainName = args.remove(0);
 
         final Method main;
         try {
-            final var mainModule = bootPath.layer().modules()
-                    .stream().filter(m -> m.getPackages().contains(mainPkg))
-                    .findFirst()
-                    .orElseThrow();
-            final var mainClass = mainModule.getClassLoader().loadClass(mainName);
-            main = mainClass.getDeclaredMethod("main", String[].class);
+            main = Class.forName(mainName).getDeclaredMethod("main", String[].class);
         } catch (Exception e) {
             throw new Exception("Failed to find main class \"" + mainName + "\"", e);
         }
 
-        main.invoke(null, new Object[] { copy.stream().flatMap(arg -> toArgs(arg).stream()).toArray(String[]::new) });
+        main.invoke(null, new Object[] { args.toArray(String[]::new) });
     }
 
-    private static Stream<String> find(List<String> args, List<String> copy, Predicate<String> test) {
-        return args.stream()
-                .filter(test)
-                .peek(copy::remove);
+    private static List<String> findValues(List<String> args, String argument) {
+        var lst = new ArrayList<String>();
+        String val;
+        while ((val = findValue(args, argument)) != null) lst.add(val);
+        return lst;
+    }
+
+    private static String findValue(List<String> args, String argument) {
+        int idx = args.indexOf(argument);
+        if (idx >= 0) {
+            args.remove(idx);
+            return args.remove(idx);
+        }
+        return null;
     }
 
     private static ModuleLayer.Controller installModulePath(Path[] path) throws Throwable {
